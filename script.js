1ca6e2dcadd235aa02f13a0eb7c6c3cf119ce81a
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-07-14 11:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,16 +26,6 @@ class Teacher extends Person {
 		console.log(`${this.name} is teaching`);
 	}
 }
-const person=new Person("John",25);
-const person=new Person("John",30);
-const student =new Student("John",30);
-student.study();
-const student =new Student("Alice",30);
-student.study();
-const teacher =new Teacher("John",30);
-teacher.teach();
-const teacher =new Teacher("Alice",30);
-teacher.teach();
 
 
 // Do not change the code below this line
